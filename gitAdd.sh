dca7bfce4ffb02d51a0c2295f696c478d0e977ca
Luis Gomez <38658851+gomezla056@users.noreply.github.com>
2020-01-30 12:27:33 -0800

    Add index bound check and correct input type check

--- gitAdd.sh
+++ gitAdd.sh
@@ -1,44 +1,59 @@
 #!/bin/bash
 
-file=''
+file=()
 
 getUnstagedFiles () {
-	local lineNumber=$(git status | grep -n "Changes not staged" | cut -d ":" -f1)
-	file=($(git status | tail +$lineNumber | grep "modified: " $file | cut -d " " -f4))
+        local lineNumber=$(git status | grep -n "Changes not staged" | cut -d ":" -f1)
+        if [[ $lineNumber ]]
+        then
+                file=($(git status | tail +$lineNumber | grep "modified: " $file | cut -d " " -f4))
+        fi
 }
 
 printUnstagedFiles () {
-	echo "Modified files: "
-	echo "${file}"
-	for j in "${!file[@]}"
-	do
-		echo -e "\033[0;36m[$j]\033[0m \033[0;31m${file[j]}\033[0m"
-	done
+        echo "Modified files: "
+        for j in "${!file[@]}"
+        do
+                echo -e "\033[0;36m[$j]\033[0m \033[0;31m${file[j]}\033[0m"
+        done
 }
 
 deleteElement () {
-	unset file[$1]
-	file=( "${file[@]}" )
+        unset file[$1]
+        file=( "${file[@]}" )
 }
 
 getUnstagedFiles
-printUnstagedFiles
 
-echo "Choose a file to add or press 'Q' to quit: "
-read index
-while [[ $index != "Q" && $index != "q" ]]
-do
-	if [[ $index -ge ${#file[@]} || $index -lt 0 ]]
-	then
-		maxLength="$((${#file[@]} - 1))"
-		echo "Number must be between 0 and $maxLength"
-	else
-		git add "${file[index]}"
-		echo -e "File \033[0;31m${file[j]}\033[0m has been added.\n"
-		deleteElement "$index"
-		printUnstagedFiles
-	fi
-	read index
-done
-
+if [[ ${#file[@]} -eq 0 ]]
+then
+        echo "No files to add"
+else
+        printUnstagedFiles
+        echo "Choose a file to add or press 'Q' to quit: "
+        read index
+        while [[ $index != "Q" && $index != "q" ]]
+        do
+                if [[ $index -ge ${#file[@]} || $index > 0 ]]
+                then
+                        maxLength="$((${#file[@]} - 1))"
+                        echo "Input must be an integer between 0 and $maxLength"
+                elif ! [[ $index =~ ^[0-9]+$ ]]
+                then
+                        echo "Input must be an integer"
+                else
+                        git add "${file[index]}"
+                        echo -e "File \033[0;31m${file[j]}\033[0m has been added.\n"
+                        deleteElement "$index"
+                        if [[ ${#file[@]} -eq 0 ]]
+                        then
+                                echo "All files have been added"
+                                break
+                        fi
+                        printUnstagedFiles
+                        echo "Choose a file to add or press 'Q' to quit: "
+                fi
+                read index
+        done
+fi
 echo "Goodbye, my dude"
